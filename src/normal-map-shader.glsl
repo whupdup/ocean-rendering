eed900d69701c57fbb59fb8098cbe7a09885c4e7
whupdup <lenny.litvak@gmail.com>
2019-08-26 22:58:55 -0400

    Fixed normal map calculation for later use

--- src/normal-map-shader.glsl
+++ src/normal-map-shader.glsl
@@ -9,21 +9,23 @@ layout (binding = 1, rgba32f) writeonly uniform image2D normalMap;
 
 uniform int N;
 
-#define LAMBDA 1
+#define NORMAL_STRENGTH 1.0
 
 void main() {
 	const ivec2 x = ivec2(gl_GlobalInvocationID.xy);
 
-	const float d00 = imageLoad(displacement, x).y;
+	//const float d00 = imageLoad(displacement, x).y;
 	const float d10 = imageLoad(displacement, ivec2( mod(x + vec2(1, 0), vec2(N)) )).y;
 	const float dn10 = imageLoad(displacement, ivec2( mod(x + vec2(-1, 0), vec2(N)) )).y;
 	const float d01 = imageLoad(displacement, ivec2( mod(x + vec2(0, 1), vec2(N)) )).y;
 	const float dn01 = imageLoad(displacement, ivec2( mod(x + vec2(0, -1), vec2(N)) )).y;
 
-	const float dXdx = (dn10 - 2 * d00 + d10);
-	const float dYdy = (dn01 - 2 * d00 + d01);
+	//const float dXdx = (dn10 - 2 * d00 + d10);
+	//const float dYdy = (dn01 - 2 * d00 + d01);
+	
+	//imageStore(normalMap, x, vec4(dXdx, dYdy, d00, 1.0));
 
-	imageStore(normalMap, x, vec4(dXdx, dYdy, d00, 1.0));
+	imageStore(normalMap, x, vec4(vec3(dn10 - d10, dn01 - d01, 1.0 / NORMAL_STRENGTH), 1.0));
 }
 
 #endif
